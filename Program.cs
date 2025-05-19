4ef0dee91338d04db0ea6aac19be9ea71d5482f9
Hadi3918 <hadi.sharifzadeh1378@gmail.com>
2025-05-19 20:23:43 +0330

    feat(swagger): add swagger to the project

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
